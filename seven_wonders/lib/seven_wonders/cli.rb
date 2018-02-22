c6eea0804e6b70b1d8b3eb07c634a344ed7a7158
Chaim Reisman <crreisman@gmail.com>
2018-02-22 16:01:56 +0000

    Cleared the extras

---
+++ seven_wonders/lib/seven_wonders/cli.rb
@@ -0,0 +1,45 @@
+require_relative 'seven_wonders'
+require_relative 'scraper'
+class SevenWonders::CLI
+
+  def initialize
+    SevenWonders::Scraper.get_page
+  end
+
+  def call
+    puts "The Seven Wonders of the Ancient World"
+    list_names_with_numbers
+    menu
+    goodbye
+  end
+
+  def menu
+    puts "Enter the number of the one you'd like to learn more about or to list them  again type list or type exit."
+    input = nil
+    while input != "exit"
+      input = gets.strip
+      if input == "list"
+        list_names_with_numbers
+      elsif  input == "exit"
+        return
+      else
+        get_info_by_number(input.to_i)
+      end
+    end
+  end
+
+  def goodbye
+    puts "Have a great day!"
+  end
+
+
+  def list_names_with_numbers
+    SevenWonders::Wonder.all.each.with_index do |wonder,index|
+      puts "#{index + 1}. #{wonder.name}"
+    end
+  end
+
+  def get_info_by_number(i)
+    puts  SevenWonders::Wonder.all[i - 1].info
+  end
+end
---
+++ seven_wonders/lib/seven_wonders/seven_wonders.rb
@@ -0,0 +1,22 @@
+require_relative 'scraper'
+class SevenWonders::Wonder
+  attr_accessor :name, :info
+  @@all = []
+
+  def initialize(wonder_hash)
+    wonder_hash.each{|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
+  def self.create_from_scraper(wonders_array)
+    wonders_array.each do |wonders|
+      new_wonder = SevenWonders::Wonder.new(wonders)
+    end
+    self.all
+  end
+
+  def self.all
+    @@all
+  end
+
+end
